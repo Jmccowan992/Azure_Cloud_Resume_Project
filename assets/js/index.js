fe00dcc59cebe8eaa3d5f9ff195cec5daf53fe7a
Jordan Mccowan <127155773+Jmccowan992@users.noreply.github.com>
2024-07-15 09:19:13 -0400

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://jrmcccloudapi.azurewebsites.net/api/http_trigger?");
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
-}
-
-updateCounter();
